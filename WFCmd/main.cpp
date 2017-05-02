5c815016ebb3e665b2608ff6503a93cb604be115
heyx3 <manning.w@husky.neu.edu>
2017-05-01 21:50:44 -0400

    Finish most of WFCmd

--- WFCmd/main.cpp
+++ WFCmd/main.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <algorithm>
 
 #include <State.h>
 
 
 //Takes in an input image (where each pixel is an unsigned int), and some parameters.
 //Generates an output image via WFC.
+
+//If "-shellMode" argument is passed, then nothing is printed to standard output
+//    except for the final resulting image.
 
 //The program settings are read from standard input.
 //The resulting image is sent to standard output as a series of rows separated by line breaks.
@@ -21,19 +25,26 @@
 1 4 4 4 4 4 1       <----- etc.
 1 4 3 4  3 4 1      <----- Spaces don't mess anything up; use as many of them as you want here!
                     <----- Empty line; the image is completed.
-TODO: MORE DATA HERE.
+3                   <----- The pattern width.
+3                   <----- The pattern height.
+true                <----- Make the input periodic horizontally?
+true                <----- Make the input periodic vertically?
+false               <----- Also use rotated copies of the input?
+false               <----- Also use reflected copies of the input?
 32                  <----- Width of the output image.
 32                  <----- Height of the output image.
 41231               <----- PRNG seed.
 false               <----- Make the output periodic horizontally?
 true                <----- Make the output periodic vertically?
-2                   <----- Violation clear size (enter 0 to just fail on a constraint violation)
+2                   <----- Violation clear size (enter 0 to simply fail on a constraint violation).
+-1                  <----- Number of iterations (anything below 0 is interpreted as "infinity").
     [File end on previous line]
 */
 
-//Outputs the following error codes:
-// 0: everything went fine.
-// 1: input image is badly-formed.
+//Returns the following error codes:
+// 0: success
+// 1: input image is badly-formed
+// 2: the WFC constraint was violated and Violation Clear Size was 0
 
 
 //Parses a pixel, expected to be a uint.
@@ -73,12 +84,28 @@ bool TryParsePixelRow(const std::string& rowAsString, std::vector<WFC::Pixel>& o
         }
     }
 
+    if (!number.empty())
+    {
+        WFC::Pixel p;
+        if (!TryParsePixel(number, p))
+            return false;
+
+        outValue.push_back(p);
+        number.clear();
+    }
+
     return true;
 }
 
 
-WFC::InputData GetDataFromStream(std::istream& input, std::ostream& output)
+//The following two functions get data from the given input stream.
+//If the given output stream isn't null, it writes instructions to the user as it gets input.
+
+WFC::InputData GetDataFromStream(std::istream& input, std::ostream* output)
 {
+    //Let input stream parse "true" as true and "false" as false.
+    std::boolalpha(input);
+
     const size_t maxUint = std::numeric_limits<size_t>().max();
 
     //Get the input image data.
@@ -87,18 +114,23 @@ WFC::InputData GetDataFromStream(std::istream& input, std::ostream& output)
         size_t nPixelsPerRow = maxUint;
         std::vector<std::vector<WFC::Pixel>> pixelsByRow;
 
-        output << "Enter rows of input image data, where each pixel is a non-negative integer.\n" <<
-                  "To finish the image, enter an empty line.\n" <<
-                  "Malformed lines are ignored.\n\n";
+        if (output != nullptr)
+        {
+            *output << "Enter rows of input image data, where each pixel is a non-negative integer.\n" <<
+                       "To finish the image, enter an empty line.\n" <<
+                       "Malformed lines are ignored.\n\n";
+        }
 
         std::string imageLine;
-        input >> imageLine;
+        std::getline(input, imageLine);
+
         std::vector<WFC::Pixel> pixelRow;
         while (!imageLine.empty())
         {
             if (!TryParsePixelRow(imageLine, pixelRow))
             {
-                output << "Row is badly formed: \"" << imageLine << "\"\nExiting program...\n\n";
+                if (output != nullptr)
+                    *output << "Row is badly formed: \"" << imageLine << "\"\nExiting program...\n\n";
                 exit(1);
             }
 
@@ -107,7 +139,8 @@ WFC::InputData GetDataFromStream(std::istream& input, std::ostream& output)
             {
                 if (pixelRow.size() == 0)
                 {
-                    output << "Input image's first row is empty! Exiting program...\n\n";
+                    if (output != nullptr)
+                        *output << "Input image's first row is empty! Exiting program...\n\n";
                     exit(1);
                 }
                 else
@@ -118,7 +151,8 @@ WFC::InputData GetDataFromStream(std::istream& input, std::ostream& output)
             //Otherwise, make sure the length is same as the last row.
             else if (pixelRow.size() != nPixelsPerRow)
             {
-                output << "Row is wrong length: \"" << imageLine << "\"\nExiting program...\n\n";
+                if (output != nullptr)
+                    *output << "Row is wrong length: \"" << imageLine << "\"\nExiting program...\n\n";
                 exit(1);
             }
 
@@ -126,52 +160,203 @@ WFC::InputData GetDataFromStream(std::istream& input, std::ostream& output)
             pixelsByRow.push_back(pixelRow);
             pixelRow.clear();
 
-            input >> imageLine;
+            imageLine.clear();
+            std::getline(input, imageLine);
         }
 
         //Convert to the Array2D<Pixel>.
-        inputPixels = WFC::Array2D<WFC::Pixel>(nPixelsPerRow, pixelsByRow.size());
+        inputPixels = WFC::Array2D<WFC::Pixel>((int)nPixelsPerRow, (int)pixelsByRow.size());
         for (WFC::Vector2i inputPos : WFC::Region2i(inputPixels.GetDimensions()))
             inputPixels[inputPos] = pixelsByRow[inputPos.y][inputPos.x];
     }
 
-    //TODO: Finish. Update example file above.
+    WFC::Vector2i patternSize;
+    if (output != nullptr)
+        *output << "Enter the width of each pattern:\n";
+    input >> patternSize.x;
+    if (output != nullptr)
+        *output << "Enter the hight of each pattern:\n";
+    input >> patternSize.y;
+
+    bool periodicX, periodicY;
+    if (output != nullptr)
+        *output << "Make the input periodic horizontally? (true/false)\n";
+    input >> periodicX;
+    if (output != nullptr)
+        *output << "Make the input periodic vertically? (true/false)\n";
+    input >> periodicY;
+
+    bool rotateInput, reflectInput;
+    if (output != nullptr)
+        *output << "Also use rotated copies of the input? (true/false)\n";
+    input >> rotateInput;
+    if (output != nullptr)
+        *output << "Also use reflected copies of the input? (true/false)\n";
+    input >> reflectInput;
+
+    return WFC::InputData(inputPixels, patternSize, periodicX, periodicY,
+                          rotateInput, reflectInput);
 }
 WFC::State InitializeStateFromStream(const WFC::InputData& wfcInput,
-                                     std::istream& input, std::ostream& output)
+                                     std::istream& input, std::ostream* output)
 {
     //Let input stream parse "true" as true and "false" as false.
     std::boolalpha(input);
 
     WFC::Vector2i size;
-    output << "Enter the width of the output image:\n";
+    if (output != nullptr)
+        *output << "Enter the width of the output image:\n";
     input >> size.x;
-    output << "Enter the height of the output image:\n";
+    if (output != nullptr)
+        *output << "Enter the height of the output image:\n";
     input >> size.y;
 
-    output << "Enter the PRNG seed:\n";
+    if (output != nullptr)
+        *output << "Enter the PRNG seed:\n";
     unsigned int seed;
     input >> seed;
 
-    output << "Make the output periodic horizontally? (true/false)\n";
+    if (output != nullptr)
+        *output << "Make the output periodic horizontally? (true/false)\n";
     bool periodicX, periodicY;
     input >> periodicX;
-    output << "Make the output periodic vertically? (true/false)\n";
+    if (output != nullptr)
+        *output << "Make the output periodic vertically? (true/false)\n";
     input >> periodicY;
 
     size_t violationClearSize;
-    output << "Enter the violation clear size (0 for failure on violations):\n";
+    if (output != nullptr)
+        *output << "Enter the violation clear size (0 for failure on violations):\n";
     input >> violationClearSize;
 
     return WFC::State(wfcInput, size, seed, periodicX, periodicY, violationClearSize);
 }
 
 
-int main()
+void PrintOutput(const WFC::State& state, std::ostream& stream)
 {
-    auto inputData = GetDataFromStream(std::cin, std::cout);
+    //Convert each element to a string, and find the longest string.
 
-    //TODO: Finish.
+    std::vector<std::vector<std::string>> elementsByRow(state.Output.GetDimensions().y);
+    for (size_t y = 0; y < state.Output.GetDimensions().y; ++y)
+        elementsByRow[y].resize(state.Output.GetDimensions().x);
 
-    return 0;
+    size_t maxLength = 0;
+    for (WFC::Vector2i pos : WFC::Region2i(state.Output.GetDimensions()))
+    {
+        auto& pixel = state.Output[pos];
+
+        std::string elementStr;
+        if (pixel.Value.HasValue)
+        {
+            elementStr = std::to_string(pixel.Value.Value);
+        }
+        else if (pixel.ColorFrequencies.GetSize() == 0)
+        {
+            elementStr = "x";
+        }
+        else
+        {
+            pixel.ColorFrequencies.DoToEach(
+                [&elementStr](const WFC::Pixel& key)
+                {
+                    if (elementStr.size() > 0)
+                        elementStr += "|";
+                    elementStr += std::to_string(key);
+                });
+        }
+
+        maxLength = std::max(maxLength, elementStr.size());
+        elementsByRow[pos.y][pos.x] = elementStr;
+    }
+
+    //Now print the elements, keeping the output evenly-spaced.
+    for (size_t y = 0; y < elementsByRow.size(); ++y)
+    {
+        if (y > 0)
+            stream << "\n";
+
+        size_t linePos = 0,
+               targetLinePos = 0;
+        for (size_t x = 0; x < elementsByRow[y].size(); ++x)
+        {
+            //Move up to the correct indentation.
+            while (linePos < targetLinePos)
+            {
+                stream << ' ';
+                linePos += 1;
+            }
+
+            //Print the element.
+            auto& elementStr = elementsByRow[y][x];
+            stream << elementStr;
+            linePos += elementStr.size();
+
+            //Advance the indentation.
+            targetLinePos += maxLength + 1;
+        }
+    }
+}
+
+
+int main(int argc, char* argv[])
+{
+    //Parse command-line options.
+    bool shellMode = false;
+    for (int i = 1; i < argc; ++i)
+        if (std::string(argv[i]) == "-shellMode")
+            shellMode = true;
+
+    auto outputStream = (shellMode ? nullptr : &std::cout);
+
+    auto inputData = GetDataFromStream(std::cin, shellMode ? nullptr : &std::cout);
+    if (!shellMode)
+        std::cout << "\n\n";
+    auto wfcState = InitializeStateFromStream(inputData, std::cin, shellMode ? nullptr : &std::cout);
+
+    int _nIterations;
+    if (!shellMode)
+        std::cout << "Enter the number of iterations (anything less than 0 is interpreted as \"infinity\":\n";
+    std::cin >> _nIterations;
+
+    int errorCode = 0;
+    size_t nIterations = (_nIterations < 0 ?
+                              std::numeric_limits<size_t>::max() :
+                              (size_t)_nIterations + 1);
+    WFC::List<WFC::Vector2i> failedPoses;
+    while (nIterations > 0)
+    {
+        nIterations -= 1;
+        WFC::Nullable<bool> result = wfcState.Iterate(failedPoses);
+
+        if (result.HasValue)
+        {
+            if (result.Value)
+            {
+                if (!shellMode)
+                    std::cout << "Finished!\n\n";
+                break;
+            }
+            else
+            {
+                if (!shellMode)
+                {
+                    std::cout << "Failed at the following output pixels:\n";
+                    for (size_t i = 0; i < failedPoses.GetSize(); ++i)
+                        std::cout << "{" << failedPoses[i].x << "," << failedPoses[i].y << "} ";
+                    std::cout << "\n\n";
+                }
+
+                errorCode = 2;
+                break;
+            }
+        }
+    }
+
+    PrintOutput(wfcState, std::cout);
+
+    if (!shellMode)
+        std::cout << "\n\n";
+
+    return errorCode;
 }
