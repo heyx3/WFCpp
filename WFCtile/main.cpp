2da669ec5a6455991afad06fab947547010187f4
Billy <manning.w27@gmail.com>
2019-04-24 21:13:22 -0400

    Set up WFCtile project skeleton and data files

---
+++ WFCtile/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <algorithm>
+
+#include <Tiled/State.h>
+
+
+//Takes in some input tile-sets as text files, and some parameters.
+//"Tile-set" here means a 2D rectangle of chars.
+//Outputs to stdout, a 2D grid of chars by assembling tiles together.
+//Each output char is separated by a space (or line break).
+
+//The tilesets and parameters file are expected to be alongside this executable;
+//    you can change this by passing the directory as an argument: -dir C:\foo\bar
+
+//Returns the following error codes:
+//TODO: Write.
+
+
+
+int main(int argc, char* argv[])
+{
+
+}
