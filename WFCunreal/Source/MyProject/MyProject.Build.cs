e62f6fa44c89a514cf8a08ec658c19a393e384ed
heyx3 <manning.w27@gmail.com>
2022-03-22 01:01:45 -0400

    Add Unreal project skeleton to experiment with WFC

---
+++ WFCunreal/Source/MyProject/MyProject.Build.cs
@@ -0,0 +1,28 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class MyProject : ModuleRules
+{
+	public MyProject(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		//WFC codebase is compiled within this project, not imported as a DLL.
+		PrivateDefinitions.Add("WFC_STATIC=1");
+		PrivateIncludePaths.Add("../../WFC++");
+		PrivateIncludePaths.Add("../../WFC++/Tiled3D");
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+		
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
